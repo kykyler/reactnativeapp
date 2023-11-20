cfb444b9495e2e660928891a3b4dc573ca9fdf5a
Kyler Axten <60053380+kykyler@users.noreply.github.com>
2023-11-19 22:40:28 -0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,22 +1,30 @@
-import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native';
-import ToDoList from './ToDoList';
 import ToDoForm from './ToDoForm';
+import React, { useState } from 'react';
+import ToDoList from './ToDoList';
+
 
 function App() {
-  // State management for tasks
   const [tasks, setTasks] = useState([
     { text: 'Do laundry', completed: true },
     { text: 'Go to gym', completed: false },
     { text: 'Walk dog', completed: true }
   ]);
 
+  const addTask = (taskText) => {
+    if (tasks.find(task => task.text === taskText)) return; 
+
+    setTasks([...tasks, { text: taskText, completed: false }]);
+  };
+
   return (
     <SafeAreaView>
       <ToDoList tasks={tasks} />
-      <ToDoForm />
+      <ToDoForm addTask={addTask} />
     </SafeAreaView>
   );
 }
+
+
 
 export default App;
